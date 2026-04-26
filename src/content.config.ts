cacab90223a0f7a3dd15d94c582406819c0501ae
Cashews939 <skm79925@googlemail.com>
2026-04-26 08:11:29 +0200

    Add thumbnail image and update content configuration for 'Lorem Ipsum'

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,15 @@
 import { defineCollection, z } from 'astro:content';
-// 1. Loader importieren
 import { glob } from 'astro/loaders'; 
 
 const wissen = defineCollection({
-  // 2. Den Loader hinzufügen. Er sucht im Ordner src/content/wissen nach .md Dateien
   loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/wissen" }),
-  schema: z.object({
+  schema: ({ image }) => z.object({ // <-- Wichtig: { image } hier hinzufügen
     title: z.string(),
     description: z.string(),
-    pubDate: z.coerce.date(), // 'coerce' macht das Datum-Format flexibler
+    pubDate: z.coerce.date(),
     category: z.string(),
+    // Hier sagen wir Astro, dass thumbnail ein Bild ist:
+    thumbnail: image().optional(), 
   }),
 });
 
